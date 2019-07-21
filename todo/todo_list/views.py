27ff667350c9b71705bc8b5c1fc3db786e2a88ff
weifo <1641929622@qq.com>
2019-07-21 23:47:58 +0800

    simple todo_app using django

---
+++ todo/todo_list/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    if request.method =='POST':
+        form=ListForm(request.POST or None)
+        
+        if form.is_valid():
+            form.save()
+            all_items=List.objects.all
+            messages.success(request,('计划添加成功！'))
+            return render(request,'home.html',{"all_items":all_items})
+        
+        else:
+            all_items=List.objects.all
+            return render(request,'home.html',{'all_items':all_items})
+    else:    
+        all_items=List.objects.all
+        return render(request,'home.html',{'all_items':all_items})
+
+def delete(request,list_id):
+    item=List.objects.get(id=list_id)
+    item.delete()
+    messages.success(request,('删除成功！'))
+    return redirect('home')
+
+def cross_off(request,list_id):
+    item=List.objects.get(id=list_id)
+    item.completed=True
+    item.save()
+    return redirect('home')
+
+def uncross(request,list_id):
+    item=List.objects.get(id=list_id)
+    item.completed=False
+    item.save()
+    return redirect('home')
+
+def edit(request,list_id):
+    if request.method =='POST':
+        item=List.objects.get(id=list_id)
+        form=ListForm(request.POST or None,instance=item)
+        
+        if form.is_valid():
+            form.save()
+            all_items=List.objects.all
+            messages.success(request,('修改成功！'))
+            return redirect('home')
+    else:    
+        item=List.objects.get(id=list_id)
+        return render(request,'edit.html',{'item':item})
