06b9bc8575b7aaf37474ccd987dd73d03869dab2
weifo <1641929622@qq.com>
2019-07-23 00:48:09 +0800

    a shopping app( uncomplete)

---
+++ e_shop/shops/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category,Product
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulated_fileds={'slug':{'name',}}
+
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','slug','category','price','stock','available','created','updated','image','description']
+    list_filter=['available','created','updated','category']
+    list_editable=['price','stock','available']
+    prepopulated_fileds={'slug':{'name',}}
+    
+admin.site.register(Product,ProductAdmin)
